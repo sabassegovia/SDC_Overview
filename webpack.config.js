72cb783e27d37ea1bb365af06432d15de00778fa
Raymond Yee <haesik.yee@gmail.com>
2022-03-29 17:01:02 -0700

    fixes webpack errors, accounts for .jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "client/src", "index.js"),
+  entry: path.join(__dirname, "client/src", "index.jsx"),
   watch: true,
   output: {
     path:path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
+      {
+        test: /\.?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
       {
         test: /\.?js$/,
         exclude: /node_modules/,
