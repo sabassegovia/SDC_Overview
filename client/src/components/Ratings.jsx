e9ba4f2b2269519c91604bef2c300edae5f6b462
9gramsof <minggui.yactayo@gmail.com>
2022-04-01 12:35:42 -0700

    adding styled-components files

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -1,18 +1,79 @@
 import React from 'react';
+import styled from 'styled-components';
+import StarRatingComponent from 'react-star-rating-component';
+
+import Button from '../styles/Buttons.jsx';
+import {Title, Wrapper, Header2} from '../styles/Headers.jsx';
+import {Form, Label, Input} from '../styles/Forms.jsx';
+import {DescriptionBox, BigBox, LittleBox} from '../styles/Boxes.jsx';
 
 class Ratings extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      rating: 4.5
     }
   }
 
+  onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
+  }
+
+
   render() {
+
+    const { rating } = this.state;
+
     return (
-      <h1>ayyyy</h1>
+      <div>
+        <Wrapper>
+          <Title>
+            Ratings and Reviews
+          </Title>
+        </Wrapper>
+        <div>
+          <Header2>This is an h2 header</Header2>
+
+          <Form>
+            <Label>
+              this is a label
+              <Input placeholder="type here..."></Input>
+            </Label>
+            <Button>Submit</Button>
+          </Form>
+        </div>
+        <div>
+          <BigBox>This is a big box
+          <DescriptionBox>This is a description box
+          <LittleBox>This is a little box</LittleBox>
+          </DescriptionBox>
+          </BigBox>
+
+
+
+        </div>
+
+
+        <div>
+          <div>
+            <h3> Ratings and Review </h3>
+            <h1>
+              {rating}
+              <StarRatingComponent
+                name="rate1"
+                starCount={5}
+                value={rating}
+                onStarClick={this.onStarClick.bind(this)}
+              />
+            </h1>
+            <p>80% of reviews recommend this product</p>
+          </div>
+        </div>
+      </div>
     );
+
+
   };
 }
 
---
+++ client/src/styles/Boxes.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DescriptionBox = styled.div`
+  height: 300px;
+  width: 250px;
+  border: 1px solid #000;
+  margin: 5px;
+  padding: 10px;
+`;
+
+const BigBox = styled.div`
+  height: 500px;
+  width: 600px;
+  border: 1px solid #000;
+  margin: 5px;
+  padding: 10px;
+
+`;
+
+const LittleBox = styled.div`
+  height: 200px;
+  width: 200px;
+  border: 1px solid #000;
+  margin: 5px;
+  padding: 10px;
+`;
+
+export {DescriptionBox, BigBox, LittleBox}
