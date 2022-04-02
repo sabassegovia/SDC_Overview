06846d5127a0e152fce3055995a3b3ad4359d0f5
Raymond Yee <haesik.yee@gmail.com>
2022-04-02 16:18:48 -0700

    implements a few basic functions

--- client/src/components/QnA Components/QuestionListItem.jsx
+++ client/src/components/QnA Components/QuestionListItem.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 var QuestionListItem = (props) => {
   return (
     <div>
-      <div className = "qa-list-item-container">
-        <section className = "question">I am question</section>
-        <section className = "answer">I am answer</section>
-        <section className = "answer">I am answer</section>
-        <section className = "answer">I am answer</section>
+      <div className="qa-list-item-container">
+        <section className="question">
+          Q: Question content goes here?
+        </section>
+        <section className="answer">A: Answer to your question.</section>
+        <section className="answer">A: Another answer to your question.</section>
       </div>
 
-      <div className = "qa-list-item-helpful-container">
-        <section className = "helpful">
-          <h3>I AM HELPFUL</h3>
+      <div className="qa-list-item-helpful-container">
+        <section className="helpful">
+          <h3>Helpful? Yes (number) | Add Answer</h3>
         </section>
       </div>
     </div>
--- client/src/components/QnA.jsx
+++ client/src/components/QnA.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import axios from "axios"
+import { Axios } from "../AxiosConfig.js"
 
 import QuestionList from './QnA Components/QuestionList.jsx';
 import QuestionSearch from './QnA Components/QuestionSearch.jsx';
 
+
 class QnA extends React.Component {
   constructor(props) {
     super(props);
+    this.searchHandler = this.searchHandler.bind(this);
 
     this.state = {
       test: 'test'
     }
   }
 
+  // load the questions and answers for the current product on page
+  componentDidMount() {
+    var current = this.props.product_id;
+    Axios.get('/qa/questions', {
+      product_id: current,
+    })
+      .then(result => {
+        console.log(result.data);
+        console.log(current)
+      })
+  }
+
+  searchHandler(query) {
+    console.log('searching the API for: ', query);
+  }
+
+
   render() {
     return (
       <div>
         <h2>Questions & Answers</h2>
-        <QuestionSearch />
+        <QuestionSearch searchHandler={this.searchHandler} />
         <QuestionList />
       </div>
     );
