384973c275aae05ad0bad4c52325976646103467
Pascal <pascal9057@gmail.com>
2022-04-05 10:13:04 -0700

    random changes

--- client/src/components/OverviewComponents/Style.jsx
+++ client/src/components/OverviewComponents/Style.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import {Thumbnail, StyleThumbnail} from './OverviewStyles.jsx'
+import { FcOk } from 'react-icons/fc';
+
 
 const Style = (props) => {
   var {photos, name, original_price, sale_price, style_id} = props.style
 
   return (
     <StyleThumbnail>
+
+      {props.selected? <FcOk className = "thumbnail-checked"/> :null}
+
       <Thumbnail
+
         url = {photos[0].thumbnail_url}
         selected = {props.selected}
         onClick = {() => {props.styleOnClick(props.style)}}>
       </Thumbnail>
+
     </StyleThumbnail>
   )
 }
