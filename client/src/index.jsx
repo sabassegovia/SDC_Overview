4e36e978aa1cd9849669d2ea820092d8dbd1cce6
Pascal <pascal9057@gmail.com>
2022-04-11 16:10:23 -0700

    last commit before merge

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,14 +15,14 @@ import {ColumnContainer, RowContainer, AlignmentWrapper, Theme, MainHeader} from
 
 const BigContainer = styled(RowContainer)`
   justify-content: center;
-  min-width: 1000px;
+  min-width: 1400px;
   opacity: ${props => props.isRender ? 1 : 0};
   transition: opacity 1.5s ease-in;
 `
 const AppContainer = styled(ColumnContainer)`
   background-color: #FAFAFA;
   margin: 10px;
-  min-width: 1000px;
+  min-width: 1400px;
   max-width: 1400px;
   border-radius: 12px;
 `
